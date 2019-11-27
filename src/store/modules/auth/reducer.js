c07d055b39c8c68b405703fb18cfa0a3cd00cabb
Anderson Ganhara <aganhara@gmail.com>
2019-11-27 14:36:47 -0300

    Adiionado configuração de store.

---
+++ src/store/modules/auth/reducer.js
@@ -0,0 +1,11 @@
+const INITIAL_STATE = {
+  token: null,
+  signed: false,
+};
+
+export default function auth(state = INITIAL_STATE, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
